ae6f786419b286c37aa75b040f6a915ef6cd3fa0
luuvanphat <luuphat402@gmail.com>
2025-04-24 11:49:38 +0700

    commit 1: init khoi tao chuong trinh ql san pham

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Ứng dụng quản lý sản phẩm</h1>
+      <p>Dự án đã được khởi tạo thành công với React và Vite</p>
+    </div>
+  );
+}
+
+export default App;
